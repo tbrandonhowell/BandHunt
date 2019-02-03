4fef2a0e4d97a44c594cbeee69d0a0b94bbaa94e
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-02-03 12:27:51 -0500

    bh20190203a

--- assets/javascript/songkick.js
+++ assets/javascript/songkick.js
@@ -14,6 +14,19 @@ var songkickAPI = function() {
     }).then(function(response) { // what to do when the API returns data. in this case we're putting the api response into this function
         console.log('location search response came back!!!');
         console.log(response);
+        console.log("response.resultsPage.totalEntries: " + response.resultsPage.totalEntries);
+        if (response.resultsPage.totalEntries === 0) { // if the town is not in songkick's database, do this:
+            console.log("no match for this town");
+            // update the table contents
+            var newMessage = '<tr><td colspan="4">';
+            newMessage = newMessage + '<h3>Did you spell that right?<h3>';
+            newMessage = newMessage + '<h4>Unfortunately we weren\'t able find your town in the SongKick database. Try searching for the next largest town or city.</h4>';
+            console.log(newMessage);
+            $("#eventTable").empty();
+            $("#eventTable").append(newMessage);
+            metroArea = ""; // reset metro area
+            return; // exit out of the function
+        };
         metroArea = response.resultsPage.results.location[0].metroArea.id;
         console.log("metroArea: " + metroArea);
         metroAreaName = response.resultsPage.results.location[0].metroArea.displayName;
@@ -21,11 +34,30 @@ var songkickAPI = function() {
     //============================
     // GET PERFORMANCES FROM THE METRO AREA
     }).then(function() {
+        console.log("metroArea: " + metroArea);
+        if (!metroArea) {
+            console.log("metroArea is undefined, printing notification");
+            return; // prevent the API call from happening if we don't have a metroArea with which to call the API
+        }
         return $.ajax({ // the request to the API
             url: 'https://api.songkick.com/api/3.0/metro_areas/' + metroArea + '/calendar.json?apikey=A1gWVGMiT6nFuDTr', // where the data is
             method: 'GET'
         }).then(function(response) { // what to do when the API returns data. in this case we're putting the api response into this function        
             $("#eventTable").empty(); // blow out the events table
+            // check to see if there are actually events for this area. if not, display a message
+            console.log("repsonse.resultsPage.results.event:");
+            console.log(response.resultsPage.results.event);
+            if (!response.resultsPage.results.event) {
+                console.log("no events for this metro area");
+                // update the table contents
+                var newMessage = '<tr><td colspan="4">';
+                newMessage = newMessage + '<h3>Not much happening in your neck of the woods?<h3>';
+                newMessage = newMessage + '<h4>Unfortunately we weren\'t able to find any shows in your area. Try searching for the next largest town or city.</h4>';
+                console.log(newMessage);
+                $("#eventTable").append(newMessage);
+                return; // exit out of the function
+            };
+            // if there are events, start building out the table
             var newFirstRow = '<tbody><tr><th>Band</th><th>Venue</th><th>Date</th><th>Vote</th></tr>'; 
             $("#eventTable").append(newFirstRow); // replace first row in events table
             console.log('metro performances response came back!!!');
@@ -38,17 +70,22 @@ var songkickAPI = function() {
                 for (x=0;x<eventArray[i].performance.length;x++) {// within each event, loop through the artists array and print out a row for each artist
                     // convert the date
                     var cleanDate = moment(eventArray[i].start.date, "YYYY-MM-DD");
-                    cleanDate = moment(cleanDate).format("MMM D");
-                    // create the new row
-                    var insertRow = '<tr>';
-                    // var insertRow = insertRow + '<td><img src="https://images.sk-static.com/images/media/profile_images/artists/' + eventArray[i].performance[x].artist.id + '/avatar" width="50" height="50" class="img-thumbnail"></td>'; // image would go here
-                    var insertRow = insertRow + '<td><a href="' + eventArray[i].uri + '">' + eventArray[i].performance[x].artist.displayName + '</a></td>';
-                    var insertRow = insertRow + '<td><a href="' + eventArray[i].venue.uri + '">' + eventArray[i].venue.displayName + '</a></td>';
-                    var insertRow = insertRow + '<td>' + cleanDate + '</td>';
-                    var insertRow = insertRow + '<td><a href="#">Favorite Link</a></td>';
-                    var insertRow = insertRow + '</tr>';
-                    $("#eventTable").append(insertRow);
-                    artistNameArray.push(eventArray[i].performance[x].artist.displayName); // add the artist name to artistNameArray[] that we'll use to query spotify
+                    console.log("Is this event in the past? : " + moment(cleanDate).isBefore(moment(), 'day'));
+                    // need some logic here - only create the new row if the event date is in the future
+                    // this is needed b/c sometimes songkick API returns have random old dates in them
+                    if ( moment(cleanDate).isBefore(moment(), 'day') === false ) { // if the date is not in the past, then add to the table
+                        cleanDate = moment(cleanDate).format("MMM D"); // strip out year from date printout
+                        // create the new row
+                        var insertRow = '<tr>';
+                        // var insertRow = insertRow + '<td><img src="https://images.sk-static.com/images/media/profile_images/artists/' + eventArray[i].performance[x].artist.id + '/avatar" width="50" height="50" class="img-thumbnail"></td>'; // image would go here
+                        var insertRow = insertRow + '<td><a href="' + eventArray[i].uri + '">' + eventArray[i].performance[x].artist.displayName + '</a></td>';
+                        var insertRow = insertRow + '<td><a href="' + eventArray[i].venue.uri + '">' + eventArray[i].venue.displayName + '</a></td>';
+                        var insertRow = insertRow + '<td>' + cleanDate + '</td>';
+                        var insertRow = insertRow + '<td><a href="#">Favorite Link</a></td>';
+                        var insertRow = insertRow + '</tr>';
+                        $("#eventTable").append(insertRow);
+                        artistNameArray.push(eventArray[i].performance[x].artist.displayName); // add the artist name to artistNameArray[] that we'll use to query spotify
+                    } // close the "if" check for past date
                 } // close FOR loop
             } // close FOR loop
             $("eventTable").append("</tbody>"); // close the tbody so the CSS height still works
